8d3c4edb752def6b689efee8b49aff5b436aabca
Avinash Prasad <avionmission@gmail.com>
2022-05-01 21:07:43 +0530

    Added country code changer widget, OTP widget, TextWatcher functions, Skip button, modified login page design and more

--- app/src/main/java/com/jobdoneindia/jobdone/PhoneLoginActivity.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/PhoneLoginActivity.kt
@@ -1,13 +1,29 @@
 package com.jobdoneindia.jobdone
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.chaos.view.PinView
 import com.hbb20.CountryCodePicker
+import org.w3c.dom.Text
 
 class PhoneLoginActivity : AppCompatActivity(), CountryCodePicker.OnCountryChangeListener {
 
     private var ccp: CountryCodePicker? = null
     private var selected_country_code: String = "+91"
+    private var firstPinView: PinView? = null
+    private var phoneLayout: ConstraintLayout? = null
+    private var phoneEditText: EditText? = null
+    private var otpTitle: TextView? = null
+    private var skipButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +31,56 @@ class PhoneLoginActivity : AppCompatActivity(), CountryCodePicker.OnCountryChang
         supportActionBar?.hide()
 
         ccp = findViewById(R.id.ccp)
+        phoneEditText = findViewById<EditText>(R.id.editTextPhone)
+        firstPinView = findViewById<View>(R.id.firstPinView) as PinView
+        phoneLayout = findViewById<View>(R.id.phoneLayout) as ConstraintLayout
+        otpTitle = findViewById<View>(R.id.otp_title) as TextView
+        skipButton = findViewById<View>(R.id.skip_button) as Button
+
+        // country code picker
         ccp!!.setOnCountryChangeListener(this)
+
+        // Text Watcher
+        phoneEditText!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length == 10) {
+                    Toast.makeText(this@PhoneLoginActivity, "10 digits detected", Toast.LENGTH_SHORT).show()
+                    phoneLayout!!.visibility = View.GONE
+                    firstPinView!!.visibility = View.VISIBLE
+                    otpTitle!!.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        // OTP Watcher
+        firstPinView!!.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length == 5) {
+                    Toast.makeText(this@PhoneLoginActivity, "5 digit pin", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        // Skip button
+        skipButton!!.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onCountrySelected() {
--- app/src/main/java/com/jobdoneindia/jobdone/SplashActivity.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/SplashActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
