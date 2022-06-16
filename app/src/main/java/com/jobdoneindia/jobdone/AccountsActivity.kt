1b2496cfaf15e6509e4810b7cb9f22365c7fb3d1
Avinash Prasad <avionmission@gmail.com>
2022-06-16 14:42:44 +0530

    Started building UI AccountsActivity.kt and created EditProfileActivity.kt

--- app/src/main/java/com/jobdoneindia/jobdone/AccountsActivity.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/AccountsActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AccountsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accounts)
+        supportActionBar?.hide()
 
         // Initialize and assign variables
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationDrawer)
+        val editButton = findViewById<FloatingActionButton>(R.id.edit_button)
 
         // Set Accounts selected
         bottomNavigationView.selectedItemId = R.id.menuAccount
@@ -52,6 +55,11 @@ class AccountsActivity : AppCompatActivity() {
 
             }
             false
+        }
+
+        // edit button (fab) onClick listener
+        editButton.setOnClickListener {
+            startActivity(Intent(applicationContext, EditProfileActivity::class.java))
         }
 
     }
---
+++ app/src/main/java/com/jobdoneindia/jobdone/EditProfileActivity.kt
@@ -0,0 +1,37 @@
+package com.jobdoneindia.jobdone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+
+        // Add back button in Action Bar
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Done fab button
+        val doneButton: FloatingActionButton = findViewById(R.id.done_button)
+        doneButton.setOnClickListener {
+            startActivity(Intent(applicationContext, AccountsActivity::class.java))
+        }
+
+
+    }
+    // Set up function for back button in Action Bar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                android.R.id.home -> {
+                    this.finish()
+                    return true
+                }
+            }
+            return super.onOptionsItemSelected(item)
+    }
+}
