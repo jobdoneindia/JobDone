0c6aef63329eab6c43b787080eeb25ec5570295f
Yuvraj Chhetri <yuvichh01@gmail.com>
2022-07-24 11:50:28 +0530

    added phone auth and logged in function

--- app/src/main/java/com/jobdoneindia/jobdone/activity/LoginActivity.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/activity/LoginActivity.kt
@@ -5,16 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.jobdoneindia.jobdone.R
 
 class LoginActivity: AppCompatActivity() {
 
     private var skipButton: Button? = null
+    private var firebaseUser: FirebaseUser?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        if(firebaseUser != null){
+            val intent = Intent(this,DashboardActivity::class.java)
+            startActivity(intent)
+        }
+
         skipButton = findViewById<View>(R.id.skip_button) as Button
 
         val phoneButton = findViewById<Button>(R.id.phn_button)
--- app/src/main/java/com/jobdoneindia/jobdone/activity/SplashActivity.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/activity/SplashActivity.kt
@@ -6,9 +6,13 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.jobdoneindia.jobdone.R
 
 class SplashActivity: AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -17,8 +21,13 @@ class SplashActivity: AppCompatActivity() {
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+
+
+
+
+                startActivity(intent)
+                finish()
+
         }, 2000)
     }
 }
