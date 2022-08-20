2ea219a2b6b06baba81b39171c03d8b7145a7bd6
Avinash Prasad <avionmission@gmail.com>
2022-08-20 15:13:21 +0530

    DP display in ChatUserList.kt activity, and feature to see details when clicking on DP

--- app/src/main/java/com/jobdoneindia/jobdone/activity/User.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/activity/User.kt
@@ -4,17 +4,20 @@ class User {
     var Username : String? = null
     var uid : String? = null
     var phoneNumber: String? = null
+    var url: String? = null
 
     constructor(){}
 
     constructor(
         uid:String?,
         name: String?,
-        phoneNumber: String?
+        phoneNumber: String?,
+        url: String?
     ){
         this.uid = uid
         this.Username = name
         this.phoneNumber = phoneNumber
+        this.url = url
 
     }
 
