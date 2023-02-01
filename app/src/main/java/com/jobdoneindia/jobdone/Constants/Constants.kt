4b9c48a5748976da4407eccf0f469d257de76c8a
jobdoneindia <jobdoneindia@gmail.com>
2023-02-01 10:21:01 +0530

    PROJECT CONNECTED TO FIREBASE JOBDONEOFFICIAL by siraj😎

--- app/src/main/java/com/jobdoneindia/jobdone/Constants/Constants.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/Constants/Constants.kt
@@ -3,7 +3,7 @@ package com.jobdoneindia.jobdone.Constants
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAJezYwsU:APA91bG8GjWwSPvdP9A-l865uY1_yd0N7grzMqGBcxlyZSx4PpnwnOlE9nS6lEoOmUcW4F4YUrzKP-iZh2nC-7soE4GiY6lV_TLh3QUnevnwZwbHEa2Pvqgdfeeuq74Sdnu7Afg61Sb5"  //GET FIREBASE SERVER KEY FROM FIREBASE
+        const val SERVER_KEY = "AAAAuQ3Qxog:APA91bF5hb31P8DuKyAySb_TUel7xjlxBNMbfSbWPkEG36MIWDJUH0m2nFBBE6TCKUOfG_AZ2yjK6wtnqPZldXYeqdZFc-aZrMw_60Hzy1w0AkPKsDAxXBMoB7uNXISbqgMhcR_GUgyX"  //GET FIREBASE SERVER KEY FROM FIREBASE
         const val CONTENT_TYPE = "application/json"
     }
 }
