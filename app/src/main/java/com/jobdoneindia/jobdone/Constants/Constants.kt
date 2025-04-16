e5cd0fd33c94bffd8cc0fd0048600b3c38858071
jobdoneindia <104266790+jobdoneindia@users.noreply.github.com>
2025-04-16 14:07:04 +0530

    Update Constants.kt

--- app/src/main/java/com/jobdoneindia/jobdone/Constants/Constants.kt
+++ app/src/main/java/com/jobdoneindia/jobdone/Constants/Constants.kt
@@ -3,7 +3,7 @@ package com.jobdoneindia.jobdone.Constants
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAuQ3Qxog:APA91bF5hb31P8DuKyAySb_TUel7xjlxBNMbfSbWPkEG36MIWDJUH0m2nFBBE6TCKUOfG_AZ2yjK6wtnqPZldXYeqdZFc-aZrMw_60Hzy1w0AkPKsDAxXBMoB7uNXISbqgMhcR_GUgyX"  //GET FIREBASE SERVER KEY FROM FIREBASE
+        const val SERVER_KEY = "AAAAuQ3Qxog:APA91bF5hb31P8DuKyAySb_TUel7xjlxBNMbfSbWPkEG36MIFc-aZrMw_60Hzy1w0AkPKsDAxXBMoB7uNXISbqgMhcR_GUgyX"  //GET FIREBASE SERVER KEY FROM FIREBASE
         const val CONTENT_TYPE = "application/json"
     }
 }
